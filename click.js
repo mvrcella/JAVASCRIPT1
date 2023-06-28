2d9ff83edf8dbd2a780dc116e50c7958f5b123f9
marcella <82300924+mvrcella@users.noreply.github.com>
2023-06-28 10:41:40 -0300

    Add files via upload

---
+++ click.js
@@ -0,0 +1,8 @@
+function umClique(){
+  document.getElementById("imagem").src="calopsita.jpg";
+  document.getElementById("log").innerHTML += "troca para a foto da calopsita<br>";
+}
+function doisCliques(){
+  document.getElementById("imagem").src="desenho.jpg";
+  document.getElementById("log").innerHTML += "troca para o desenho<br>";
+}
